1aa5f1f9900c0a4b0df397e068e52b5c560dce72
David Wallace Croft <david@CroftSoft.com>
2026-02-25 19:53:04 -0600

    secret key

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+use ::anyhow::Result;
+use ::iroh::{Endpoint, SecretKey};
+use ::rand::rngs::OsRng;
+
+#[::tokio::main]
+async fn main() -> Result<()> {
+  let secret_key: SecretKey = SecretKey::generate(OsRng);
+
+  println!("> our secret key: {secret_key}");
+
+  let endpoint: Endpoint = Endpoint::builder()
+    .secret_key(secret_key)
+    .discovery_n0()
+    .bind()
+    .await?;
+
+  println!("> our node id: {}", endpoint.node_id());
+
+  Ok(())
 }
